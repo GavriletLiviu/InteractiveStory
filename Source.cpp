550fe8881b093b2e29fe6791a65d3d675ec823b6
Liviu Cristian Gavrilet <liviu.gavrilet@gmail.com>
2022-11-03 00:03:07 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,157 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string MC = "Jonathan", animal = "cat", place = "jungle", SC = "woman", basket, reel, poles, man, CouncilOfLords, youngwoman;
+	string AMC, Aanimal, Aplace, ASC, Abasket, Areel, Apoles, Aman, ACouncilOfLords, Ayoungwoman;
+	cout << " this is a semi-interactive story based on user answering a set of questions \n";
+	while (AMC != MC)
+	{
+		cout << "Please type name of main character ";
+		cin >> AMC;
+		cout << "\n";
+		if (AMC != MC)
+			cout << "wrong answer \n";
+	}
+	cout << MC << " walked for several hours without a glimpse of any sign of life.\n";
+	cout << "Suddenly, something moved in the bush and a small animal with a yellow - striped tail flashed down a barely visible track. \n";
+	while (Aanimal != animal)
+	{
+		cout << "please type animal species = ";
+		cin >> Aanimal;
+		cout << "\n";
+		if (Aanimal != animal)
+			cout << "wrong answer \n";
+	}
+	cout << "'A " << animal << " ' thought " << MC << " \n";
+
+	while (Aplace != place)
+	{
+		cout << " please input place ( such as deset, jungle) ";
+		cin >> Aplace;
+		cout << "\n";
+		if (Aplace != place)
+			cout << "wrong answer \n";
+	}
+	while (ASC != SC)
+	{
+		cout << "Please input name of second character = ";
+		cin >> ASC;
+		cout << "\n";
+		if (ASC != SC)
+			cout << "wrong answer \n";
+	}
+	cout << "  - Maybe it will lead me to some people? He dived through the thick foliage. Just as he lost sight of the " << place << ", he heard a sharp scream. \n";
+	cout << " He stopped, cocked his head, and tried to locate the source of the sound. \n";
+	cout << "Directly ahead, he heard another shrill cry for help. \n";
+	cout << "Pushing up an incline and through a mass of branches and vines, he clawed his way forward and stumbled onto a wider path. \n";
+	cout << "As he rounded a sharp bend in the trail, " << MC << " ran full tilt into the side of a burly man. \n ";
+	cout << " - Out of my way, chump!  bellowed the man, brushing him aside like a gnat. \n";
+	cout << "Dazed, " << MC << " looked up and saw two men dragging " << SC << " , kicking and yelling, down the trail. \n";
+	cout << "By the time he caught his breath, the trio had disappeared. \n";
+	cout << "Certain that he couldn’t free " << SC << "alone, " << MC << " ran back up the trail looking for help. \n";
+	cout << "A clearing opened and he saw a group of people gathered around a big tree – beating it with sticks. \n ";
+	cout << MC << " ran up and grabbed the arm of a man who was obviously the supervisor.\n";
+	cout << " - Please sir, help! gasped " << MC << " . - Two men have captured a woman and she needs help! \n ";
+	cout << " - Dont be alarmed, the man said gruf?y. \n ";
+	cout << " - She’s under arrest. Forget her and move along, we’ve got work to do. \n";
+	cout << " - Arrest? said " << MC << ", still huf?ng. \n";
+	cout << " - She didn’t look like, uh, like a criminal." << MC << " Jonathan wondered, if she was guilty, why did she cry so desperately for help? \n";
+	cout << " - Pardon me, sir, but what was her crime?\n";
+	cout << " - Huh?  snorted the man with irritation.  - Well, if you must know, she threatened the jobs of everyone working here.\n";
+	cout << " - She threatened people’s jobs? How did she do that?  asked <<MC<< . \n";
+	cout << "Glaring down at his ignorant questioner, the supervisor motioned for " << MC << " to come over to a tree where workers busily pounded away at the trunk. \n";
+	cout << " Proudly, he said, \n";
+	cout << " - We are tree workers. We knock down trees for wood by beating them with these sticks.\n";
+	cout << " - Sometimes a hundred people, working round-the-clock, can knock down a good-sized tree in less than a month. \n ";
+	cout << "The man pursed his lips and carefully brushed a speck of dirt from the sleeve of his handsomely cut coat. \n ";
+	cout << " - " << SC << " came to work this morning with a sharp piece of metal attached to the end of her stick. \n";
+	cout << " She cut down a tree in less than an hour – all by herself! Think of it! \n";
+	cout << " - Such an outrageous threat to our traditional employment had to be stopped. \n";
+	cout << MC << "'s eyes widened, aghast to hear that this woman was punished for her creativity. \n";
+	cout << "Back home, everyone used axes and saws for cutting trees. \n";
+	cout << " That’s how he got the wood for his own boat. \n";
+	cout << " But her invention, exclaimed " << MC << "allows people of all sizes and strengths to cut down trees.\n";
+	cout << " - Won’t that make it faster and cheaper to get wood and make things? \n ";
+	cout << " - What do you mean? the man said angrily. \n";
+	cout << " - How could anyone encourage an idea like that? This noble work can’t be done by any weakling who comes along with some new idea. \n";
+	cout << " But sir, said " << MC << " trying not to offend, \n";
+	cout << " - these good tree workers have talented hands and brains. \n";
+	cout << " - They could use the time saved from knocking down trees to do other things. They could make tables, cabinets, boats, or even houses! \n";
+	cout << " - Listen, you, the man said with a menacing look, \n";
+	cout << " - the purpose of work is to have full and secure employment – not new products.\n";
+	cout << " The tone of his voice turned ugly. \n";
+	cout << " - You sound like some kind of troublemaker. Anyone who supports that infernal woman is trouble. Where are you from? \n";
+	cout << MC << " replied anxiously : \n";
+	cout << "I don’t even know " << SC << " and I don’t mean any trouble, sir. I’m sure you’re right. Well, I must be going. \n";
+	cout << "With that, " << MC << " turned back the way he came, hurrying down the path. His ?rst encounter with the people of the island left him feeling very nervous.\n";
+	cout << " THE END !!!  \n ";
+	cout << "The trail widened a bit as it cut through the dense " << place << ". \n";
+	cout << " The midday sun burned hot overhead when " << MC << " found a small lake. \n";
+	cout << "As he scooped up some water to refresh himself, " << MC << " heard someone’s voice warning, \n ";
+	cout << " - I wouldn’t drink the water if I were you. \n";
+	cout << " Please input secondary character name = \n"; cin >> man;
+	cout << MC << " looked around and saw an old " << man << " kneeling at the shore, cleaning a few tiny ? sh on a plank. \n";
+	cout << "Name three fishing supplies : \n";
+	cin >> basket >> reel >> poles;
+	cout << "Beside him was a " << basket << ", a " << reel << ", and three " << poles << " propped up in the mud, each dangling a line in the water. ";
+	cout << " - Is the ? shing good?  -inquired Jonathan politely. \n";
+	cout << " Without bothering to look up, the man replied, somewhat crossly : \n ";
+	cout << " - Nope. These little critters were all I got today. \n ";
+	cout << "He proceeded to ?llet the fish and to drop them into a hot skillet that was set over a smoldering fire. \n ";
+	cout << " The fish sizzling in the pan smelled delicious. \n ";
+	cout << MC << " spotted the rough yellow-striped cat that he had followed, already picking at scraps of ?sh. \n ";
+	cout << "His mouth watered. \n";
+	cout << MC << " , who considered himself an accomplished ? sherman, asked, \n";
+	cout << " - What did you use for bait? \n";
+	cout << " - The " << man << "looked up at Jonathan thoughtfully.\n";
+	cout << " - There’s nothing wrong with my bait, sonny. I’ve caught the best of what’s left in this lake. \n ";
+	cout << "Sensing a solitary mood in this ?sherman, Jonathan thought he might learn more by just remaining silent awhile. \n";
+	cout << " Eventually, the old " << man << " beckoned him to sit beside the fire to share some fish and a little bread. \n";
+	cout << "Jonathan devoured his meal hungrily, though he felt guilty about taking a portion of this man’s meagre lunch. \n ";
+	cout << "After they finished, Jonathan kept quiet and, sure enough, the " << man << " began to talk : \n ";
+	cout << " - Years ago there were some really big fish to catch here, \n ";
+	cout << "  the " << man << " said wistfully.  \n ";
+	cout << " - But they’ve all been caught. Now the little ones are all that’s left. \n";
+	cout << " - But the little ones will grow, won’t they? \n ";
+	cout << " asked Jonathan.  \n";
+	cout << " He stared at the lush grasses growing in the shallow waters along the shore where many fish might lurk.  \n ";
+	cout << " No. People take all the fish, even the little ones. Not only that, people dump rubbish into the far end of the lake. See that thick scum along the far side? \n ";
+	cout << " Jonathan looked perplexed.  \n";
+	cout << " - Why do others take your fish and dump rubbish in your lake? \n";
+	cout << " - Oh, no, \n ";
+	cout << " said " << man << ".  \n";
+	cout << " - this isn’t my lake. It belongs to everyone – just like the forests and the streams. \n";
+	cout << " - These fish belong to everyone ...\n ";
+	cout << MC << " paused, ";
+	cout << " - including me? He began to feel a little less guilty about sharing a meal that he had no part in making.  \n";
+	cout << " - Not exactly,\n ";
+	cout << "the man replied. \n ";
+	cout << " - What belongs to everyone really belongs to no one – that is, until a fish bites my hook. Then it’s mine. \n ";
+	cout << " - I don’t get it, \n";
+	cout << "said Jonathan, frowning in confusion. Half speaking to himself, he repeated: \n";
+	cout << " - The fish belong to everyone, which means that they really belong to no one, until one bites your hook. \n";
+	cout << " - Then, the fish is yours? But do you do anything to take care of the fish or to help them grow? \n ";
+	cout << " - Of course not! \n";
+	cout << " - the man said with a snort of derision.  \n ";
+	cout << " - Why should I care for the fish just so someone else can come over here at any time and catch them? \n";
+	cout << " - If someone else gets the fish or pollutes the lake with rubbish, then there goes all my effort! \n ";
+	cout << " With a mournful glance at the water, " << man << " added sadly,  \n ";
+	cout << " -I wish I really did own the lake. Then Id make sure that the fish were well tended. I’d care for the lake just like the cattleman who manages the ranch over in the next valley. \n ";
+	cout << " -I’d breed the strongest, fattest fish and you can bet that no fish rustlers or garbage dumpers would get past me. I’d make sure of that. \n ";
+	cout << " Please input name of evil empire :  \n ";
+	cin >> CouncilOfLords; cout << "\n";
+	cout << " - Who manages the lake now? \n ";
+	cout << "interrupted Jonathan. The weathered face of the fisherman grew hard: \n ";
+	cout << " - The lake is run by " << CouncilOfLords << ".Every four years, the Lords are elected to the Council.Then the Council appoints a managerand pays him from my taxes.The ?sh manager is supposed to watch out for too much ?shing and dumping. \n ";
+	cout << " - The funny thing is, friends of " << CouncilOfLords << " get to ?sh and dump as they please. \n ";
+	cout << " The two sat and watched the wind stir a pattern of ripples across the silver lake. Jonathan noticed the " << animal << " sitting erect, snif?ng and staring at a ?sh head on his plate. \n ";
+	cout << " He tossed the head and " << animal << " caught it neatly with one hooked paw.  \n ";
+	cout << " This feline looked tough, with one ear torn from some old battle. \n ";
+	cout << " Mulling over the old fisherman’s tale, Jonathan asked : \n ";
+	cout << " - Is the lake well-managed? \n ";
+	cout << " - See for yourself, \n ";
+	cout << "the " << man << " grumbled:  \n";
+	cout << " - Look at the size of my puny catch. It seems that the fish get smaller as the manager’s salary gets bigger. \n ";
+	
+}
